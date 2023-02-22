3a94c090d59f0e01d176e67b44964c0bfe81f484
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-22 18:58:39 +0300

    Add or remove class depending on API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,28 @@
+$( document ).ready(function () {
+    let amenityList = [];
+    $('input[type=checkbox]').change (function () {
+      let name = $(this).attr('data-name');
+        if ($(this).is(':checked')) {
+      amenityList.push(name);
+        } else {
+      amenityList = amenityList.filter(amen => amen !== name);
+        }
+      $('.amenities h4').text(amenityList.join(', '));
+    });
+
+    // display red circle on top right of page if status is OK
+    $.ajax({
+      type: 'GET',
+      url: 'http://0.0.0.0:5001/api/v1/status/',
+      dataType: 'json',
+      success: function (data) {
+        if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+  
+      } else {
+      $('#api_status').removeClass('available');
+        }
+      }
+    });
+      
+});
